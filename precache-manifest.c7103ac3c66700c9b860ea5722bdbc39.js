6b27d4163b3f8b1e7ab1a2e1df70274e7a55feaf
James Glenn <james.glenn@hp.com>
2019-12-12 23:22:56 -0600

    Updates

---
+++ precache-manifest.c7103ac3c66700c9b860ea5722bdbc39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86972f9987b4861689d0b078f414529",
+    "url": "/ignite-application/index.html"
+  },
+  {
+    "revision": "cbb70f1b3863700f79dc",
+    "url": "/ignite-application/static/css/main.b361bb5d.chunk.css"
+  },
+  {
+    "revision": "da61a822b943ea9cce9b",
+    "url": "/ignite-application/static/js/2.88a1b510.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ignite-application/static/js/2.88a1b510.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cbb70f1b3863700f79dc",
+    "url": "/ignite-application/static/js/main.5bf69784.chunk.js"
+  },
+  {
+    "revision": "996dbb1acd1bffe8c925",
+    "url": "/ignite-application/static/js/runtime-main.e025cfb3.js"
+  }
+]);
